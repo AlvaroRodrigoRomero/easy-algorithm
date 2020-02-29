eec341463c5befa374e77f87cd97b6e58a47ef47
inkirer <alvaro.rodrigo.romero@gmail.com>
2020-02-29 18:48:23 +0100

    Added breadcrumbs

---
+++ src/components/breadcrumbs.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+
+export default class BreadCrumbs extends Component {
+    generateBreadCrumbs(pathname) {
+        var paths = pathname.split("/");
+        paths = paths[paths.length - 1] === "" ? paths.slice(0, paths.length - 1) : paths;
+        paths = paths[1] === "" ? paths.slice(1) : paths;
+        var breadcrumb = paths.map((path, index) => {
+            var arrow = index !== paths.length - 1 ? " > " : " ";
+            if (index === 0)
+                return ({ path: "/", name: "Home" });
+
+            var url = paths.slice(0, index + 1).join('/');
+            return ({ path: url, name: path.replace(/%20/g, " ") });
+        });
+        return (breadcrumb);
+    }
+
+    render() {
+        var breadcrumb = this.generateBreadCrumbs(window.location.pathname);
+        if (breadcrumb.length > 1)
+            return (
+                <Breadcrumb>
+                    {breadcrumb.map((item, index) => {
+                        return <Breadcrumb.Item active={index == breadcrumb.length - 1}
+                            href={item.path}>{item.name}
+                        </Breadcrumb.Item>
+                    })}
+                </Breadcrumb>
+            )
+        else
+            return (null)
+    }
+}
--- src/components/container.js
+++ src/components/container.js
@@ -4,16 +4,18 @@ import BubbleSort from '../views/sorting/bubbleSort';
 import QuickSort from '../views/sorting/quickSort';
 import Home from '../views/home';
 import NotFound from '../views/404'
+import BreadCrumbs from '../components/breadcrumbs'
 
 export default class Container extends Component {
     render() {
         return (
             <div>
+                <BreadCrumbs />
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/bubblesort" component={BubbleSort} />
                     <Route exact path="/quicksort" component={QuickSort} />
-                    <Route path="*" component={NotFound} />
+                    {/* <Route path="*" component={NotFound} /> */}
                 </Switch>
             </div>
         )
